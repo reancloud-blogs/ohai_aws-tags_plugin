327e758b97fcf478e5055da050ec09bcfee3bd5e
Phil Watts <phil@reancloud.com>
2016-04-12 09:42:07 -0400

    Initial

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'chef-ohai-hints'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures chef-ohai-hints'
+long_description 'Installs/Configures chef-ohai-hints'
+version          '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: chef-ohai-hints
+# Recipe:: default
+#
+# Copyright (C) 2016 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
